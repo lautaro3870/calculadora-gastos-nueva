ce3502115f3c56e25246af3d49fafb0e2b45e476
lautaro3870 <lautarodisbro@gmail.com>
2024-06-15 12:42:01 +0200

    feat: reporte por rango de fechas

--- src/components/Calculoreporte.tsx
+++ src/components/Calculoreporte.tsx
@@ -1,24 +1,15 @@
-import {
-  Box,
-  MenuItem,
-  Select,
-  SelectChangeEvent,
-  Button,
-} from "@mui/material";
+import { Box } from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import React, { useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import { getLocalItems } from "../funciones/GetLocalItems";
 import { Form } from "react-bootstrap";
 import sumar from "../funciones/Sumar";
-import { Label } from "@mui/icons-material";
-import meses from "../Meses";
-import categorias from "../Categorias";
 
 export default function CalculoReporte() {
-  const [mes, setMes] = useState<string>("");
   const [listado, setListado] = useState(getLocalItems());
   const [subtotal, setSubTotal] = useState<number>(0);
-  const [categoria, setCategoria] = useState<string>("");
+  const fromInput = useRef<HTMLInputElement>(null);
+  const toInput = useRef<HTMLInputElement>(null);
 
   const columns: GridColDef[] = [
     { field: "gasto", headerName: "Gasto", width: 120 },
@@ -26,85 +17,48 @@ export default function CalculoReporte() {
     { field: "fecha", headerName: "Fecha", width: 150 },
   ];
 
-  const handleChangeSelect = (event: SelectChangeEvent) => {
-    console.log(event.target.value);
-    setMes(event.target.value);
-    const valor = parseInt(event.target.value);
+  const convertirFecha = (fecha: string) => {
+    const partes = fecha.split("-");
+    const año = partes[0];
+    const mes = partes[1];
+    const día = partes[2];
+
+    return `${mes}/${día}/${año}`;
   };
 
-  const handleChangeCategorias = (event: SelectChangeEvent) => {
-    console.log(event.target.value);
-    setCategoria(event.target.value);
-  };
-
-  const filtrar = () => {
-    const mesSeleccionado = mes;
-    const categoriaSeleccionada = categoria;
-
-    const a = getLocalItems();
-
-    const filteredListConMeses = a.filter((item: any) => {
-      const dateParts = item.fecha.split("/");
-      const month = parseInt(dateParts[1]);
-      return month === parseInt(mesSeleccionado);
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const from = fromInput.current?.value || "";
+    const to = toInput.current?.value || "";
+    const items = getLocalItems();
+    let filteredItems = items.filter((i: any) => {
+      return i.fecha >= convertirFecha(from) && i.fecha <= convertirFecha(to);
     });
-
-    const filteredList = filteredListConMeses.filter((item: any) => {
-      return item.categoria === categoriaSeleccionada;
-    });
-
-    setListado(filteredList);
-    const resultado = sumar(filteredList);
-    setSubTotal(resultado);
+    setListado(filteredItems);
+    setSubTotal(sumar(filteredItems));
   };
 
   return (
     <div>
       <br />
       <Form>
-        <Select
-          size="small"
-          style={{ marginRight: "10px" }}
-          onChange={handleChangeCategorias}
-        >
-          {categorias.map((i) => {
-            return (
-              <MenuItem selected key={i} value={i}>
-                {i}
-              </MenuItem>
-            );
-          })}
-        </Select>
-        <Select
-          size="small"
-          onChange={handleChangeSelect}
-          value={mes}
-          label="Mes"
-        >
-          {meses.map((i: any) => (
-            <MenuItem key={i.id} value={i.id}>
-              {i.mes}
-            </MenuItem>
-          ))}
-        </Select>
-        <Button
-          variant="outlined"
-          style={{ marginLeft: "10px" }}
-          onClick={filtrar}
-        >
-          Filtrar
-        </Button>
-        <Button
-          variant="outlined"
-          style={{ marginLeft: "10px" }}
-          color="error"
-          onClick={() => {
-            setListado(getLocalItems());
-            setSubTotal(0);
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginBottom: 20,
           }}
         >
-          Limpiar
-        </Button>
+          <input ref={fromInput} type="date" className="form-control" />
+          <input ref={toInput} type="date" className="form-control" />
+        </div>
+        <button
+          className="btn btn-primary"
+          style={{ marginLeft: "10px" }}
+          onClick={handleSubmit}
+        >
+          Filtrar
+        </button>
         <label style={{ marginLeft: "10px" }}>
           Subtotal: {subtotal.toFixed(2)}
         </label>
